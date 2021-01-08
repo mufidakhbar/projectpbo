2b20c2bf13b7c083bd833065f5cb4db6b27f15a3
M Farhan Anfasa <72295932+mfarhananfasa@users.noreply.github.com>
2021-01-08 12:07:18 +0700

    Update Kendaraan.java

--- Kendaraan.java
+++ Kendaraan.java
@@ -5,4 +5,37 @@ public class Kendaraan
     private String plat;
     private int jenis;
     protected int uangawal = 100000;
+    
+    public Kendaraan()
+    {
+        
+    }
+    
+    public Kendaraan(String merek, String warna, String plat, int jenis)
+    {
+        this.merek = merek;
+        this.warna = warna;
+        this.plat = plat;
+        this.jenis = jenis;
+    }
+
+    public String getMerek()
+    {
+        return merek;
+    }
+    
+    public int getJenis()
+    {
+        return jenis;
+    }
+
+    public String getWarna()
+    {
+        return warna;
+    }
+
+    public String getPlat()
+    {
+        return plat;
+    }
 }
