7a993c676dcf7b0262ce0f10afebceb4484d11e6
muhammadfarisadzkia <77149409+muhammadfarisadzkia@users.noreply.github.com>
2021-01-08 17:26:25 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        Parkiran park1 = new Parkiran();
+        System.out.println("====== PARKING AREA ======\nMENU :");
+        System.out.println("1. Masukkan Kendaraan");
+        System.out.println("2. Keluarkan Kendaraan");
+        System.out.println("3. Lihat Kendaraan");
+        System.out.print("Pilihan : ");
+        
+    }   
+}
